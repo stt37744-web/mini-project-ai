b09008ca31f0d1800a5f92c41f6aead6dd8d4285
stt37744-web <stt37744@gmail.com>
2026-08-06 13:42:19 +0530

    Fix syntax error in PuzzleState constructor

--- 8puzzle_gui.py
+++ 8puzzle_gui.py
@@ -5,7 +5,7 @@ import random
 import time
 fromtyping import List, Tuple, Optional
 class PuzzleState:
-    def __init__(self, board: List        [List[int]], moves: int = 0, prev: Optional['PuzzleState'] = None):
+    def __init__(self, board: List[List[int]], moves: int = 0, prev: Optional['PuzzleState'] = None):
         self.board = board  
         self.moves = moves
         self.prev =  prev  
